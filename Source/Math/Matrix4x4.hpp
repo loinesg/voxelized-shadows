a88c3b3829f0d297e82b0d392885ad44b6d172fe
George Loines <loinesg@gmail.com>
2017-02-21 11:52:59 +0000

    [Fix] Math: Convert Matrix4x4 to column-major order

--- Source/Math/Matrix4x4.hpp
+++ Source/Math/Matrix4x4.hpp
@@ -10,20 +10,23 @@ using namespace std;
 
 struct Matrix4x4
 {
+    // Elements stored in column-major order
     float elements[16];
 
 public:
     Matrix4x4();
-
-    // Allows use of matrix[i][j] syntax
-    float* operator[] (int i) { return elements + (i * 4); }
-    const float* operator[] (int i) const { return elements + (i * 4); }
+    
+    float get(int row, int column) const;
+    
+    void set(int row, int column, float value);
+    void setRow(int row, float* values);
+    void setCol(int col, float* values);
 
     static Matrix4x4 identity();
     static Matrix4x4 translation(const Vector3 &t);
     static Matrix4x4 rotation(const Quaternion &q);
     static Matrix4x4 scale(const Vector3 &s);
-
+    
     // Constructs a matrix for performing a scale, then a rotation, then a translation.
     static Matrix4x4 trs(const Vector3 &translation, const Quaternion &rotation, const Vector3 &scale);
 
@@ -47,4 +50,3 @@ Matrix4x4 operator - (const Matrix4x4 &a, const Matrix4x4 &b);
 Vector4 operator * (const Matrix4x4 &mat, const Vector4 &v);
 
 ostream& operator << (ostream &os, Matrix4x4 &mat);
-istream& operator >> (istream &is, Matrix4x4 &mat);
