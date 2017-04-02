230f3404bc341aa6cc26d30f06bfaa4f3b093423
George Loines <loinesg@gmail.com>
2017-04-02 22:21:10 +0100

    [Fix] Animation: Prevent error accumulation over time
    Limited precision resulted in animated objects drifting from their
    actual axis over time. Clamp rotations and cache axis vectors to
    prevent this from occurring.

--- Source/Scene/Animation.cpp
+++ Source/Scene/Animation.cpp
@@ -1,11 +1,21 @@
 #include "Animation.hpp"
 
+#include <math.h>
+
 Animation::Animation(MeshInstance* meshInstance, Vector3 rotationSpeed)
     : meshInstance_(meshInstance),
     rotationSpeed_(rotationSpeed)
 {
     // Animated mesh instances are made non-static
     meshInstance->makeNonStatic();
+    
+    // Store the original rotation
+    originalRotation_ = meshInstance_->rotation();
+    
+    // Get the axis in world space
+    xAxis_ = meshInstance_->right().vec3();
+    yAxis_ = meshInstance_->up().vec3();
+    zAxis_ = meshInstance_->forward().vec3();
 }
 
 void Animation::update(float deltaTime)
@@ -15,8 +25,15 @@ void Animation::update(float deltaTime)
     float rotationY = rotationSpeed_.y * deltaTime;
     float rotationZ = rotationSpeed_.z * deltaTime;
     
+    // Add the new rotation.
+    // Wrap around at 360 degrees so that the
+    totalRotation_.x = fmod(totalRotation_.x + rotationX, 360.0);
+    totalRotation_.y = fmod(totalRotation_.y + rotationY, 360.0);
+    totalRotation_.z = fmod(totalRotation_.z + rotationZ, 360.0);
+    
     // Rotate along each axis in turn
-    meshInstance_->rotate(rotationX, meshInstance_->right().vec3());
-    meshInstance_->rotate(rotationY, meshInstance_->up().vec3());
-    meshInstance_->rotate(rotationZ, meshInstance_->forward().vec3());
+    meshInstance_->setRotation(originalRotation_);
+    meshInstance_->rotate(totalRotation_.x, xAxis_);
+    meshInstance_->rotate(totalRotation_.y, yAxis_);
+    meshInstance_->rotate(totalRotation_.z, zAxis_);
 }
--- Source/Scene/Animation.hpp
+++ Source/Scene/Animation.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Vector3.hpp"
+#include "Quaternion.hpp"
 #include "MeshInstance.hpp"
 
 class Animation
@@ -19,6 +20,22 @@ public:
     
 private:
     
+    // The animation target
     MeshInstance* meshInstance_;
+    
+    // The rotation rate, in degrees per second, around each axis
     Vector3 rotationSpeed_;
+    
+    // The original rotation of the target object.
+    // The rotation is applied from here each frame to avoid
+    // floating point error accumulation.
+    Quaternion originalRotation_;
+    
+    // The amount, in degrees, that the object has been rotated
+    Vector3 totalRotation_;
+    
+    // The world space axis to rotate around
+    Vector3 xAxis_;
+    Vector3 yAxis_;
+    Vector3 zAxis_;
 };
